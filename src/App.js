6b119651b91eeaa1e30d6818e6dcb22300d0e59e
nagi4074 <nagi4074@gmail.com>
2021-08-10 11:59:20 +0900

    inputタグの扱い

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import InputWithUseState from './components/InputWithUseState'
+import InputWithUseRef from './components/InputWithUseRef'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputWithUseState />
+      <InputWithUseRef />
     </div>
   );
 }
